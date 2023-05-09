6f0923dbfcde0d77652d978a7190841cc34df588
Cesar Padilla Lazos <cesarpadillalazos8@gmail.com>
2023-05-09 10:04:49 -0600

    catch error added

--- src/controllers/index.controllers.js
+++ src/controllers/index.controllers.js
@@ -1,6 +1,13 @@
 import {pool} from "../db.js"
 
 export const ping = async (req,res) => {
-    const [result] = await pool.query('SELECT "PONG" AS result')
-    res.json(result[0])
+    try{
+        const [result] = await pool.query('SELECT "PONG" AS result')
+        res.json(result[0])
+    }
+    catch(error){
+        return res.status(404).json({
+            message: error.message
+        })
+    }
 }
